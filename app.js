0dc0c4f12b1d7b3cb2937cfa5b6816e6f6374676
makx <delmst@yandex.ru>
2020-09-13 14:56:30 +0300

    fix app.js

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+  app.use('/', express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
 }
 const PORT = process.env.PORT || 5000;
